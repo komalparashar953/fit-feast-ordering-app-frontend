e788acbfb92e583f64e7c6f3de5714a35ce7d142
KOMAL1108 <parashark953@gmail.com>
2024-05-21 15:05:18 +0530

    added keys

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,7 +3,6 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
-import ProtectedRoute from "./auth/ProtectedRoute";
 import ManagaeRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
@@ -27,11 +26,11 @@ const AppRoutes = () => {
               }
             />
             
-            <Route element={<ProtectedRoute />}>
+            
               <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
               <Route path="/manage-restaurant" element={<Layout><ManagaeRestaurantPage /></Layout>} />
               <Route path="/order-status" element={<Layout><OrderStatusPage /></Layout>} />
-            </Route>
+          
             
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
